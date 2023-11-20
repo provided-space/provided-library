a8acc99f5683db16122f684bdb43a42784b43149
eikenHub <eikengames@gmail.com>
2023-11-20 23:54:12 +0100

    restructured

--- src/main/java/space/provided/natives/Provided.java
+++ src/main/java/space/provided/natives/Provided.java
@@ -26,69 +26,80 @@ public final class Provided {
 
     public static native void deleteAssetItem(int itemId) throws ApiException;
 
-    public static native String encrypt(String value, String key);
+    public static class Obfuscation {
 
-    public static native String decrypt(String value, String key);
+        public static native String encrypt(String value, String key);
 
-    public static native int i_a(int n1, int n2);
+        public static native String decrypt(String value, String key);
 
-    public static native int i_b(int n1, int n2);
+        public static native int i_a(int n1, int n2);
 
-    public static native int i_c(int n1, int n2);
+        public static native int i_b(int n1, int n2);
 
-    public static native int i_d(int n1, int n2);
+        public static native int i_c(int n1, int n2);
 
-    public static native int i_e(int n1, int n2);
+        public static native int i_d(int n1, int n2);
 
-    public static native int i_f(int n1, int n2);
+        public static native int i_e(int n1, int n2);
 
-    public static native int i_g(int n1, int n2);
+        public static native int i_f(int n1, int n2);
 
-    public static native int i_h(int n1, int n2);
+        public static native int i_g(int n1, int n2);
 
-    public static native int i_i(int n1, int n2);
+        public static native int i_h(int n1, int n2);
 
-    public static native int i_j(int n1, int n2);
+        public static native int i_i(int n1, int n2);
 
-    public static native long l_a(long n1, long n2);
+        public static native int i_j(int n1, int n2);
 
-    public static native long l_b(long n1, long n2);
+        public static native long l_a(long n1, long n2);
 
-    public static native long l_c(long n1, long n2);
+        public static native long l_b(long n1, long n2);
 
-    public static native long l_d(long n1, long n2);
+        public static native long l_c(long n1, long n2);
 
-    public static native long l_e(long n1, long n2);
+        public static native long l_d(long n1, long n2);
 
-    public static native long l_f(long n1, long n2);
+        public static native long l_e(long n1, long n2);
 
-    public static native long l_g(long n1, long n2);
+        public static native long l_f(long n1, long n2);
 
-    public static native long l_h(long n1, long n2);
+        public static native long l_g(long n1, long n2);
 
-    public static native long l_i(long n1, long n2);
+        public static native long l_h(long n1, long n2);
 
-    public static native long l_j(long n1, long n2);
+        public static native long l_i(long n1, long n2);
 
-    public static native float f_a(float n1, float n2);
+        public static native long l_j(long n1, long n2);
 
-    public static native float f_b(float n1, float n2);
+        public static native float f_a(float n1, float n2);
 
-    public static native float f_c(float n1, float n2);
+        public static native float f_b(float n1, float n2);
 
-    public static native float f_d(float n1, float n2);
+        public static native float f_c(float n1, float n2);
 
-    public static native float f_e(float n1, float n2);
+        public static native float f_d(float n1, float n2);
 
-    public static native double d_a(double n1, double n2);
+        public static native float f_e(float n1, float n2);
 
-    public static native double d_b(double n1, double n2);
+        public static native double d_a(double n1, double n2);
 
-    public static native double d_c(double n1, double n2);
+        public static native double d_b(double n1, double n2);
 
-    public static native double d_d(double n1, double n2);
+        public static native double d_c(double n1, double n2);
 
-    public static native double d_e(double n1, double n2);
+        public static native double d_d(double n1, double n2);
 
-    public static native MicrosoftAuthResult loginWithMicrosoft() throws MicrosoftAuthException;
+        public static native double d_e(double n1, double n2);
+
+    }
+
+    public static class Microsoft {
+
+        public static native MicrosoftAuthResult loginWithMicrosoft() throws MicrosoftAuthException;
+
+        public static native MicrosoftAuthResult loginWithToken(String refreshToken) throws MicrosoftAuthException;
+
+    }
+
 }
